0e49fc8aa2aa2f7e78e1f2de28e7c9c98e6cb5b5
Andrei Lozhkin <andrei.lozhkin@anymindgroup.com>
2023-01-29 19:47:29 +0700

    feat: add ability to upload the file

--- app/Main.hs
+++ app/Main.hs
@@ -1,61 +1,103 @@
 {-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
 module Main where
 
-import           AsciiConverter.Lib          ( Config (Config, imageColor, imageWidth),
-                                               convertToAscii, resizeImage )
+import           AsciiConverter.Lib                      ( Config (Config, imageColor, imageWidth),
+                                                           convertToAscii,
+                                                           resizeImage )
 import           AsciiConverter.Read
 
-import           Conduit                     ( runConduitRes, sinkList, takeCE,
-                                               (.|) )
+import           Conduit                                 ( runConduitRes,
+                                                           sinkList, takeCE,
+                                                           (.|) )
 
-import           Control.Exception           ( SomeException, try )
-import           Control.Monad               ( unless )
+import           Control.Exception                       ( SomeException, try )
+import           Control.Monad                           ( unless )
 import           Control.Monad.IO.Class
 
-import           Data.Aeson                  ( ToJSON )
+import           Data.Aeson                              ( ToJSON )
 import           Data.ByteString
-import qualified Data.ByteString.Char8       ( readInt )
+import qualified Data.ByteString.Char8                   ( readInt )
 import           Data.Foldable
 
 import           GHC.Generics
 
-import           Graphics.Image              ( Image, RGB, VS )
+import           Graphics.Image                          ( Image, RGB, VS )
 
-import           Network.HTTP.Client         ( parseUrlThrow )
+import           Network.HTTP.Client                     ( parseUrlThrow )
 import           Network.HTTP.Simple
-import           Network.HTTP.Types          ( hContentLength, status400 )
-import           Network.Wai.Middleware.Cors ( simpleCors )
-import           Network.Wai.Middleware.Gzip ( def, gzip )
+import           Network.HTTP.Types                      ( hContentLength,
+                                                           status400 )
+import           Network.Wai.Middleware.Cors             ( simpleCors )
+import           Network.Wai.Middleware.Gzip             ( def, gzip )
+import           Network.Wai.Middleware.RequestSizeLimit
+import           Network.Wai.Parse                       ( FileInfo (FileInfo) )
 
 import           Web.Scotty
 
+
 maxFileSize :: Int
 maxFileSize = 1024
+
+requestSizeLimitSettings :: RequestSizeLimitSettings
+requestSizeLimitSettings = setMaxLengthForRequest (\r -> pure $ Just (fromIntegral maxFileSize * 1024) ) defaultRequestSizeLimitSettings
 
 middlewares :: ScottyM ()
 middlewares = do
     middleware simpleCors
     middleware $ gzip def
+    middleware $ requestSizeLimitMiddleware requestSizeLimitSettings
 
 routes :: ScottyM ()
 routes = do
-    get "/convert-url" convertUrl
+    post "/convert" convert
 
-convertUrl :: ActionM ()
-convertUrl = do
+getParameters :: ActionM (Bool, Int, String)
+getParameters = do
     imageUrl <- param "imageUrl" `rescue` (\_ -> return "") :: ActionM String
     color <- param "color" `rescue` (\_ -> return True) :: ActionM Bool
     width <- param "width" `rescue` (\_ -> return 100) :: ActionM Int
+    return (color, width, imageUrl)
+
+validateWidth :: Int -> ActionM ()
+validateWidth width
+    | width < 10 = status status400 >> (json $ BadRequestResponse { err = "width can't be less than 10" }) >> finish
+    | width > 300 = status status400 >> (json $ BadRequestResponse { err = "width can't be greater than 300" }) >> finish
+    | otherwise = return ()
+
+processImage :: Either String (Image VS RGB Double) -> Config -> ActionM ()
+processImage image config = do
+    case image of
+        Left _ -> status status400 >> (json $ BadRequestResponse { err = "Couldn't read the image, either the file is corrupted or has the wrong format" }) >> finish
+        Right img -> do
+            let resizedImg = resizeImage (imageWidth config) img
+            converted <- liftIO $ convertToAscii resizedImg config
+            (json $ AsciiResponse { res = converted }) >> finish
+
+convert :: ActionM ()
+convert = do
+    -- get parameters
+    (color, width, imageUrl) <- getParameters
 
     -- validate width
-    case width of
-        w | w < 10 -> status status400 >> (json $ BadRequestResponse { err = "width can't be less than 10" }) >> finish
-        w | w > 300 -> status status400 >> (json $ BadRequestResponse { err = "width can't be greater than 300" }) >> finish
-        _ -> return ()
-
+    validateWidth width
 
     case imageUrl of
-        "" -> status status400 >> (json $ BadRequestResponse { err = "imageUrl is required" }) >> finish
+        -- if no imageUrl were passed, check for files
+        "" -> do
+            files' <- files
+
+            if Data.Foldable.length files' > 0 then do
+                let imageFile = Prelude.head files'
+
+                case imageFile of
+                    (_, FileInfo _ _ fContent) -> do
+                        let config = Config{imageWidth = width, imageColor = color}
+                        let bs = toStrict fContent
+                        image <- liftIO $ AsciiConverter.Read.readImage "" bs :: ActionM (Either String (Image VS RGB Double))
+
+                        -- process image
+                        processImage image config
+            else status status400 >> (json $ BadRequestResponse { err = "No image was uploaded" }) >> finish
         _ -> do
             -- Parse url, throw an exception in case it's invalid
             req <- liftIO $ try (parseUrlThrow $ "HEAD " ++ imageUrl) :: ActionM (Either SomeException Request)
@@ -87,31 +129,23 @@ convertUrl = do
 
             let responseBody = Data.ByteString.concat response
 
-            -- -- return 400 error if file size exceeds MAX_FILE_SIZE
+            --return 400 error if file size exceeds MAX_FILE_SIZE
             unless (Data.ByteString.length responseBody `div` 1024 < maxFileSize)
                  (status status400 >> (json $ BadRequestResponse { err = "File is too big" }) >> finish)
 
-            -- -- config used for ascii converter
+            -- config used for ascii converter
             let config = Config{imageWidth = width, imageColor = color}
 
-            -- -- read image from byte string
+            -- read image from byte string
             image <- liftIO $ AsciiConverter.Read.readImage imageUrl responseBody :: ActionM (Either String (Image VS RGB Double))
 
-            case image of
-                Left _ -> status status400 >> (json $ BadRequestResponse { err = "Couldn't read the image, either the file is corrupted or has the wrong format" }) >> finish
-                Right img -> do
-                    -- process the image
-                    let resizedImg = resizeImage (imageWidth config) img
-                    converted <- liftIO $ convertToAscii resizedImg config
-                    (json $ AsciiResponse { res = converted }) >> finish
-
+            -- process image
+            processImage image config
 
 main :: IO ()
 main = do
     putStrLn "Starting Server..."
     scotty 3000 (middlewares >> routes)
-
-
 
 newtype BadRequestResponse
   = BadRequestResponse { err :: String }
--- docs/main.js
+++ docs/main.js
@@ -1,24 +1,48 @@
-convertToAscii = (e) => {
-    let imageUrl = $("#imageUrl").val()
-    let color = $("#asciiColored").attr("checked") === "checked"
+function convertToAscii(e) {
+    e.preventDefault()
+
+    form = document.querySelector('#ascii-form');
+    let formData = new FormData(form);
+
+    formData.append("width", 150)
+    formData.append("color", true)
+
+    $(form).find(".ascii-form-error").empty()
+    $(form).find(".ascii-form-error").addClass('d-none')
+
+    $(form).find("button").attr('disabled', true)
+    $(form).find("button .spinner-border").removeClass('d-none')
+    $(form).find("button .btn-text").text('Loading ...')
+    $('.ascii-art').empty()
+
 
     $.ajax({
-        url: `${window.SERVER_URL}/convert-url`,
-        type: 'GET',
-        data: {
-            imageUrl: imageUrl,
-            color: true,
-            width: 150
-        },
+        url: `${window.SERVER_URL}/convert`,
+        type: 'POST',
+        data: formData,
         accepts: { json: "application/json, text/javascript" },
         success: function (res) {
             $(".ascii-art").empty().append(res['res']);
         },
         error: function (res) {
-            console.log(res.responseJSON)
-        }
+
+            errorMessage = res.status == 400 ? res.responseJSON.err : res.responseText
+            $(form).find(".ascii-form-error").removeClass('d-none')
+            $(form).find(".ascii-form-error").text(errorMessage)
+
+        },
+        complete: function () {
+            $(form).find("button").attr('disabled', false)
+            $(form).find("button .spinner-border").addClass('d-none')
+            $(form).find("button .btn-text").text('Submit')
+        },
+        contentType: false,
+        processData: false
     });
+
+    form.reset();
 }
+
 
 handleColorSwitch = (e) => {
     let toggle = $("#asciiColored")
