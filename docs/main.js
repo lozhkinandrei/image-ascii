6b9516797211acb43a2957a6af63c8d8ea78f27c
Andrei Lozhkin <andrei.lozhkin@anymindgroup.com>
2023-02-02 22:03:34 +0700

    refactor: frontend

--- docs/main.js
+++ docs/main.js
@@ -1,39 +1,42 @@
+function resizeAscii(width) {
+    currentWidth = $(".ascii-art").width()
+    currentHeight = $(".ascii-art").height()
+    scale = width / currentWidth
+
+    $('.ascii-art').css('transform', `scale(${scale})`)
+    $('.ascii-art').css('margin-bottom', currentHeight * scale - currentHeight + 'px')
+    $('.ascii-art').css('margin-right', currentWidth * scale - currentWidth + 'px')
+}
+
 function convertToAscii(e) {
     e.preventDefault()
 
     form = document.querySelector('#ascii-form');
+
     let formData = new FormData(form);
-
-    formData.append("width", 150)
-    formData.append("color", true)
-
-    $(form).find(".ascii-form-error").empty()
-    $(form).find(".ascii-form-error").addClass('d-none')
-
-    $(form).find("button").attr('disabled', true)
-    $(form).find("button .spinner-border").removeClass('d-none')
-    $(form).find("button .btn-text").text('Loading ...')
-    $('.ascii-art').empty()
-
+    formData.set("color", $('#color').is(':checked'))
 
     $.ajax({
         url: `${window.SERVER_URL}/convert`,
         type: 'POST',
         data: formData,
         accepts: { json: "application/json, text/javascript" },
+        beforeSend: function () {
+            $(form).find(".ascii-form-error").empty()
+            $(form).find(".ascii-form-error").addClass('d-none')
+
+            $(form).find("button").attr('disabled', true)
+            $(form).find("button .spinner-border").removeClass('d-none')
+            $(form).find("button .btn-text").text('Loading ...')
+        },
         success: function (res) {
-            $(".ascii-art").append(res['res'])
-
-            currentWidth = $(".ascii-art").width()
-            currentHeight = $(".ascii-art").height()
-            scale = $('body .container .container').width() / currentWidth
-
-            $('.ascii-art').css('transform', `scale(${scale})`)
-            $('.ascii-art').css('margin-bottom', currentHeight * scale - currentHeight + 'px')
-            $('.ascii-art').css('margin-right', currentWidth * scale - currentWidth + 'px')
+            $('.ascii-art-dummy').remove()
+            const w = $('#description').width()
+            $(".ascii-art").empty().append(res['res'])
+            resizeAscii(w)
         },
         error: function (res) {
-            errorMessage = res.status == 400 ? res.responseJSON.err : res.responseText
+            errorMessage = res.status == 400 ? res.responseJSON.err : res.responseText || "Error occured"
             $(form).find(".ascii-form-error").removeClass('d-none')
             $(form).find(".ascii-form-error").text(errorMessage)
 
@@ -46,17 +49,4 @@ function convertToAscii(e) {
         contentType: false,
         processData: false
     });
-
-    form.reset();
-}
-
-
-handleColorSwitch = (e) => {
-    let toggle = $("#asciiColored")
-
-    if (toggle.attr("checked")) {
-        toggle.removeAttr("checked")
-    } else {
-        toggle.attr("checked", true)
-    }
 }
